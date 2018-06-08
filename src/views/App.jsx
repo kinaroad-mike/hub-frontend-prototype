456296a0889b4e0e05b386569ef18f2d722cd8c6
Michael Mason <michael.mason@kinaroad.com>
2018-06-08 15:42:48 +1000

    Refactor App component class into a pure function

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
 
 import routes from '../routes';
 
-export class App extends Component {
-  render () {
-    return (
-      <Grid>
-        <Grid.Row>
-          <Grid.Column textAlign="center" >
-            <h1>Kinaroad Hub</h1>
-          </Grid.Column>
-        </Grid.Row>
-        { routes.map(route => (
-          <Route key={route.path} {...route} />
-        ))}
-
-      </Grid>
-    );
-  }
-}
+export const App = () => (
+  <Grid>
+    <Grid.Row>
+      <Grid.Column textAlign="center" >
+        <h1>Kinaroad Hub</h1>
+      </Grid.Column>
+    </Grid.Row>
+    { routes.map(route => (
+      <Route key={route.path} {...route} />
+    ))}
+  </Grid>
+);
