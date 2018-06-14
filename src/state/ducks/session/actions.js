d174933ea26ba7db1acccc05648214c13e812fae
Michael Mason <michael.mason@kinaroad.com>
2018-06-14 13:30:25 +1000

    LOGIN_SUCCESS action dispatch stores token instead of all api response data

--- src/state/ducks/session/actions.js
+++ src/state/ducks/session/actions.js
@@ -1,3 +1,4 @@
+// import axios from 'axios';
 import * as types from './types';
 import { api } from '../../middlewares';
 
@@ -6,14 +7,14 @@ export { login };
 function login (email, password) {
   return (dispatch) => {
     dispatch(request());
-    api.post('/api-token-auth/', { email, password })
+    return api.post('/api-token-auth/', { email, password })
       .then((user) => {
         if (user && user.data.token) {
-          localStorage.setItem('authToken', JSON.stringify(user.data.token));
+          localStorage.setItem('authToken', user.data.token);
         }
-        dispatch(success(user));
+        dispatch(success(user.data.token));
       });
   };
   function request () { return { type: types.LOGIN_REQUEST }; }
-  function success (user) { return { type: types.LOGIN_SUCCESS, user }; }
+  function success (authToken) { return { type: types.LOGIN_SUCCESS, authToken }; }
 }
