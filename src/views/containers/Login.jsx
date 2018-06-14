1c7a3b01972bc4a83e9f1a67b9ac2b9acbc6928f
Michael Mason <michael.mason@kinaroad.com>
2018-06-14 13:29:14 +1000

    Implement sample login form validation

--- src/views/containers/Login.jsx
+++ src/views/containers/Login.jsx
@@ -3,8 +3,26 @@ import { reduxForm } from 'redux-form';
 import { LoginForm } from '../components';
 import { sessionOperations } from '../../state/ducks/session';
 
+function validate (values) {
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.email) {
+    errors.email = 'Enter an email address';
+  }
+
+  if (!values.password) {
+    errors.password = 'Enter a password';
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has any properties, redux form assumes form is invalid
+  return errors;
+}
+
 // Decorate the LoginForm component with redux form
 const LoginFormContainer = reduxForm({
+  validate,
   form: 'LoginForm'
 })(LoginForm);
 
